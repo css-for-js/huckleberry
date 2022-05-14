ff418792a94cb0affc5bfcd5b0af90facd8190f6
yuhcee <egbouchenna001@gmail.com>
2022-05-14 22:40:54 +0100

    networkDelayTime: add task description

---
+++ networkDelayTime.js
@@ -0,0 +1,16 @@
+/**
+ * **743. Network Delay Time**
+ *
+ * You are given a network of `n` nodes, labeled from `1` to `n`. You are also given `times`, a list of
+ * travel times as directed edges `times[i] = (ui, vi, wi)`, where `ui` is the source node, `vi` is the
+ * target node, and `wi` is the time it takes for a signal to travel from source to target.
+ *
+ * We will send a signal from a given node `k`. Return the time it takes for all the `n` nodes to receive
+ * the signal. If it is impossible for all the `n` nodes to receive the signal, return `-1`.
+ *
+ * @param {number[][]} times
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const networkDelayTime = (times, n, k) => {};
